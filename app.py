6a39d5574a04c54a282d01623ab3a353c618e825
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-09 20:12:26 -0300

    New endpoints added

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from instagrapi import Client
 from instagrapi.exceptions import LoginRequired
 import random
 ################################ Const ################################
-def simul_racers_data() -> dict:
+teams = [
+        'DS PENSKE', 'Jaguar TCS Racing', 'MAHINDRA RACING', 'Envision Racing',
+        'Nissan Formula E Team', 'Nissan Formula E Team', 'Avalanche Andretti Formula E',
+        'NIO 333 Racing FE Team', 'Envision Racing', 'Maserati MSG Racing', 'NEOM McLaren Formula E Team',
+        'ABT CUPRA FORMULA E TEAM', 'MAHINDRA RACING', 'Jaguar TCS Racing', 'TAG Heuer Porsche Formula E Team'
+    ]
+
+def simul_racers_data(teams):
     names = [
         "Jake Dennis", "Stoffel Vandoorne", "Sergio Camara", "Robin Frijns", 
         "Jake Hughes", "Maximilian Gunther", "Sam Bird", "Mitch Evans", 
@@ -11,13 +18,6 @@ def simul_racers_data() -> dict:
         "Norman Nato", "Jehan Daruvala", "Nyck de Vries", "Oliver Rowland", 
         "Sacha Fenestraz", "Jean-Eric Vergne", "Dan Ticktum", "Nick Cassidy", 
         "Edoardo Mortara", "Nico Müller", "Pascal Wehrlein"
-    ]
-
-    teams=[
-        'DS PENSKE', 'Jaguar TCS Racing', 'MAHINDRA RACING', 'Envision Racing', 
-        'Nissan Formula E Team', 'Nissan Formula E Team', 'Avalanche Andretti Formula E',
-        'NIO 333 Racing FE Team', 'Envision Racing', 'Maserati MSG Racing', 'NEOM McLaren Formula E Team',
-        'ABT CUPRA FORMULA E TEAM', 'MAHINDRA RACING', 'Jaguar TCS Racing', 'TAG Heuer Porsche Formula E Team'
     ]
 
     racers_data = [
@@ -40,11 +40,19 @@ def force_question(field, values):
         resp = input(f"{field}: ")
     return resp
 
-racers_data = simul_racers_data()
+racers_data = simul_racers_data(teams)
 
 app = Flask(__name__)
 
 ################################ Corredores ################################
+
+def racer(racer_name):
+    for racer in racers_data:
+        if racer["name"] == racer_name.lower().replace(" ", "-"):
+            return racer
+
+def all_racers():
+    return [racer for racer in racers_data]
 
 def top_racer():
     top_racers = []
@@ -63,12 +71,10 @@ def top_racer():
     return {"racers": top_racers}
 
 def total_points(field, var):
-    # if field == "" or var == "":
-    #     raise ValueError("Field or variable not informed")
-    # if field == "":
-    #     field = force_question("Field", ["team", "name"])
-    total = 0   
-    
+    if field == "" or var == "":
+        raise ValueError("Field or variable not informed")
+
+    total = 0
     for racer in racers_data:
         if racer[field] == var.lower().replace(" ", "-"):
             total += sum(racer["points"])
@@ -77,6 +83,12 @@ def total_points(field, var):
         field: var.lower().replace(" ", "-"),
         "total": total
         }
+
+def team_racers(team):
+    return [racer for racer in racers_data if racer["team"] == team.lower().replace(" ", "-")]
+
+def all_teams():
+    return [{team:team_racers(team)} for team in teams]
 
 ################################ Instagram ################################
 
@@ -92,6 +104,75 @@ def conn_instagram(data):
     return cl
 
 ################################ API ################################
+
+@app.route('/racer', methods=['GET'])
+def get_racer():
+    try:
+        racer_name = request.args.get("name")
+        return jsonify(racer(racer_name)), 200
+
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        app.logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
+@app.route('/racers', methods=['GET'])
+def get_all_racers():
+    try:
+        return jsonify(all_racers()), 200
+
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        app.logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
+
+@app.route('/team', methods=['GET'])
+def get_team_racers():
+    try:
+        field = request.args.get('team')
+
+        return jsonify(team_racers(field)), 200
+
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        app.logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
+
+@app.route('/teams', methods=['GET'])
+def get_all_teams_racers():
+    try:
+        return jsonify(all_teams()), 200
+
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        app.logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
+
+@app.route('/top', methods=['GET'])
+def top_racer_info():
+    try:
+        return jsonify(top_racer()), 200
+
+    except Exception as e:
+        app.logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
+
+@app.route('/points', methods=['GET'])
+def get_total_points():
+    try:
+        field = request.args.get('field')
+        var = request.args.get('var')
+
+        return jsonify(total_points(field, var)), 200
+
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        app.logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
 
 @app.route('/connect', methods=['POST'])
 def connect_instagram():
@@ -111,31 +192,6 @@ def connect_instagram():
     except Exception as e:
         app.logger.error(f"Unexpected error: {str(e)}")
         return jsonify({"error": "An unexpected error occurred"}), 500
-
-@app.route('/top', methods=['GET'])
-def top_racer_info():
-    try:
-        return jsonify(top_racer()), 200
-
-    except Exception as e:
-        app.logger.error(f"Unexpected error: {str(e)}")
-        return jsonify({"error": "An unexpected error occurred"}), 500
-    
-    
-@app.route('/points', methods=['GET'])
-def get_total_points():
-    try:
-        field = request.args.get('field')
-        var = request.args.get('var')
-
-        return jsonify(total_points(field, var)), 200
-
-    except ValueError as e:
-        return jsonify({"error": str(e)}), 400
-    except Exception as e:
-        app.logger.error(f"Unexpected error: {str(e)}")
-        return jsonify({"error": "An unexpected error occurred"}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
